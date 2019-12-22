fd1039dc092f770355ab8c415f8e630d5cfd6f76
Anders Jurisoo <jurisoo@hotmail.com>
2019-12-22 18:47:39 +0100

    Using a base clase to store private common test methods

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,28 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\PSRManipulator\PSRFile;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function samplePath($name)
+    {
+        return "tests/FileSamples/$name";
+    }
+
+    protected function userFile()
+    {
+        return PSRFile::load(
+            $this->samplePath('app/User.php')
+        );        
+    }
+    
+    protected function routesFile()
+    {
+        return PSRFile::load(
+            $this->samplePath('routes/web.php')
+        );        
+    }    
 }
